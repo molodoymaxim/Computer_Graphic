4a65e517bf3e101bd4bfa2e3108418d7760e86b2
molodoymaxim <103942325+molodoymaxim@users.noreply.github.com>
2023-05-23 15:07:56 +0300

    Add files via upload

---
+++ Lab3_baaa/KG3/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Lab3_baaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Z = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Цвет = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AnT
+            // 
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AnT.Location = new System.Drawing.Point(0, 0);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(1101, 563);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
+            this.AnT.MouseMove += new System.Windows.Forms.MouseEventHandler(this.AnT_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(75, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Осветить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X,
+            this.Y,
+            this.Z,
+            this.Цвет});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(267, 181);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // X
+            // 
+            this.X.HeaderText = "X";
+            this.X.Name = "X";
+            this.X.Width = 40;
+            // 
+            // Y
+            // 
+            this.Y.HeaderText = "Y";
+            this.Y.Name = "Y";
+            this.Y.Width = 40;
+            // 
+            // Z
+            // 
+            this.Z.HeaderText = "Z";
+            this.Z.Name = "Z";
+            this.Z.Width = 40;
+            // 
+            // Цвет
+            // 
+            this.Цвет.HeaderText = "Цвет";
+            this.Цвет.Name = "Цвет";
+            this.Цвет.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Цвет.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Цвет.Text = "Цвет...";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 260);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(169, 286);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(169, 312);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Радиус";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 286);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Высота";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Количество граней";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(822, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(279, 563);
+            this.panel1.TabIndex = 9;
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.AnT_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 563);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.AnT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Z;
+        private System.Windows.Forms.DataGridViewButtonColumn Цвет;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Lab3_baaa/KG3/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using Tao.FreeGlut;
+using System.Drawing;
+
+namespace Lab3_baaa
+{
+    public partial class Form1 : Form
+    {
+        private double _Radius=5;
+        private double _Height=5;
+        private int _Slices=5;
+        public Form1()
+        {
+            InitializeComponent();
+            AnT.InitializeContexts();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // инициализация Glut
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            // очитка окна
+            Gl.glClearColor(0, 0, 0, 1); // черный фон
+            // установка порта вывода в соотвествии с размерами элемента 
+            Gl.glViewport(0, 0, AnT.Width, AnT.Height);
+            // настройка параллельной проекции
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            const double W = 7;
+            double H = W * AnT.Height / AnT.Width;
+            Gl.glOrtho(-W, W, -H, H, -200, 200);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+            // настройка параметров OpenGL для визуализации
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            Gl.glEnable(Gl.GL_COLOR_MATERIAL);
+            Gl.glEnable(Gl.GL_LIGHTING);
+
+            Gl.glEnable(Gl.GL_LIGHT0);
+        }
+        private void Render(double x, double y)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+
+            Gl.glLoadIdentity();
+            Gl.glColor3f(1, 1, 1); // цвет фигуры - белый
+
+            Gl.glPushMatrix();
+            Gl.glTranslated(0, -1, -6);
+            Gl.glRotated((x - AnT.Height) * 180 / AnT.Height, 0, 1, 0);
+            Gl.glRotated((y - AnT.Width) * 180 / AnT.Width, 1, 0, 0);
+            Glut.glutSolidCone(_Radius, _Height, _Slices, 1);
+            Gl.glPopMatrix();
+            Gl.glFlush();
+            AnT.Invalidate();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Отключаем все предыдущие источники света
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    Gl.glDisable(Gl.GL_LIGHT0 + i);
+                }
+
+                // Получаем координаты одноточечного источника из первой строки таблицы
+                float posX = (float)Convert.ToDecimal(dataGridView1.Rows[0].Cells[0].Value);
+                float posY = (float)Convert.ToDecimal(dataGridView1.Rows[0].Cells[1].Value);
+                float posZ = (float)Convert.ToDecimal(dataGridView1.Rows[0].Cells[2].Value);
+
+                // Включаем освещение для одноточечного источника
+                Gl.glEnable(Gl.GL_LIGHT0);
+                float[] pos = { posX, posY, posZ, 1.0f };
+                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, pos);
+
+                // Устанавливаем цвет одноточечного источника из четвертой ячейки таблицы
+                Color color = (Color)dataGridView1.Rows[0].Cells[3].Value;
+                float[] lightColor = { color.R / 255.0f, color.G / 255.0f, color.B / 255.0f, 1.0f };
+                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_DIFFUSE, lightColor);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка входных данных!");
+            }
+        }
+
+        private void AnT_MouseMove(object sender, MouseEventArgs e)
+        {
+            Render(e.X, e.Y);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 3) if (DialogResult.OK == colorDialog1.ShowDialog()) dataGridView1.Rows[e.RowIndex].Cells[3].Value = colorDialog1.Color;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                _Radius = Convert.ToDouble(textBox1.Text);
+            }
+            catch (Exception)
+            {
+                _Radius = 5;
+            }
+            try
+            {
+                _Height = Convert.ToDouble(textBox2.Text);
+            }
+            catch (Exception)
+            {
+                _Height = 5;
+            }
+            try
+            {
+                _Slices = Convert.ToInt32(textBox3.Text);
+            }
+            catch (Exception)
+            {
+                _Slices = 5;
+            }
+        }
+    }
+}
